cc2f41c6e35ced91efcad564d39f1a9a202b9756
Louis De Bevere <louis.debevere@kbc.be>
2019-03-06 13:02:14 +0100

    Adds Swagger

--- shopping-list/src/main/java/com/ezgroceries/shoppinglist/ShoppingListApplication.java
+++ shopping-list/src/main/java/com/ezgroceries/shoppinglist/ShoppingListApplication.java
@@ -1,11 +1,16 @@
 package com.ezgroceries.shoppinglist;
 
+import com.ezgroceries.shoppinglist.config.ApplicationConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Import;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
 @ComponentScan("com.ezgroceries.shoppinglist.web")
+@Import(ApplicationConfig.class)
+@EnableSwagger2
 public class ShoppingListApplication {
 
 	public static void main(String[] args) {
