ffafedbabda86ae4f9b3c114769f7da7231c3255
Louis De Bevere <louis.debevere@kbc.be>
2019-03-06 12:54:41 +0100

    Add other endpoints to ShoppingListController

--- shopping-list/src/main/java/com/ezgroceries/shoppinglist/model/ShoppingList.java
+++ shopping-list/src/main/java/com/ezgroceries/shoppinglist/model/ShoppingList.java
@@ -1,15 +1,19 @@
 package com.ezgroceries.shoppinglist.model;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.UUID;
 
 public class ShoppingList {
 
     private final UUID shoppingListId;
     private final String name;
+    private List<String> ingredients;
 
     public ShoppingList(UUID shoppingListId, String name) {
         this.shoppingListId = shoppingListId;
         this.name = name;
+        this.ingredients = new ArrayList<>();
     }
 
     public UUID getShoppingListId() {
@@ -18,5 +22,13 @@ public class ShoppingList {
 
     public String getName() {
         return name;
+    }
+
+    public List<String> getIngredients() {
+        return ingredients;
+    }
+
+    public void addIngredient(String ingredient) {
+        this.ingredients.add(ingredient);
     }
 }
--- shopping-list/src/main/java/com/ezgroceries/shoppinglist/web/ShoppingListController.java
+++ shopping-list/src/main/java/com/ezgroceries/shoppinglist/web/ShoppingListController.java
@@ -2,12 +2,17 @@ package com.ezgroceries.shoppinglist.web;
 
 import com.ezgroceries.shoppinglist.model.ShoppingList;
 import java.net.URI;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Map;
 import java.util.UUID;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -28,6 +33,51 @@ public class ShoppingListController {
         ShoppingList shoppingList = new ShoppingList(id, body.get("name"));
         logger.info("Created shopping list '" + shoppingList.getName() + "' with id '" + shoppingList.getShoppingListId() + "'");
         return entityWithLocation(id).body(new ShoppingList(id, body.get("name")));
+    }
+
+    @PostMapping(path = "/{id}/cocktails")
+    @ResponseStatus(HttpStatus.OK)
+    public ResponseEntity<List<Map<String, String>>> addCocktails(@PathVariable String id, @RequestBody List<Map<String, String>> body) {
+        return ResponseEntity.ok(body.subList(0,1));
+    }
+
+    @GetMapping(path = "/{id}")
+    @ResponseStatus(HttpStatus.OK)
+    public ResponseEntity<ShoppingList> getList(@PathVariable String id) {
+        UUID uuid = UUID.fromString(id);
+        ShoppingList shoppingList = new ShoppingList(uuid, "Stephanie's birthday");
+        Arrays.asList("Tequila",
+                "Triple sec",
+                "Lime juice",
+                "Salt",
+                "Blue Curacao").forEach(shoppingList::addIngredient);
+        return ResponseEntity.ok(shoppingList);
+    }
+
+    @GetMapping
+
+    @ResponseStatus(HttpStatus.OK)
+
+    public ResponseEntity<List<ShoppingList>> getList() {
+
+        List<ShoppingList> lists = new ArrayList<>();
+        UUID uuid = UUID.fromString("4ba92a46-1d1b-4e52-8e38-13cd56c7224c");
+        ShoppingList shoppingList = new ShoppingList(uuid, "Stephanie's birthday");
+        Arrays.asList("Tequila",
+                "Triple sec",
+                "Lime juice",
+                "Salt",
+                "Blue Curacao").forEach(shoppingList::addIngredient);
+        lists.add(shoppingList);
+        uuid = UUID.fromString("6c7d09c2-8a25-4d54-a979-25ae779d2465");
+        shoppingList = new ShoppingList(uuid, "My birthday");
+        Arrays.asList("Tequila",
+                "Triple sec",
+                "Lime juice",
+                "Salt",
+                "Blue Curacao").forEach(shoppingList::addIngredient);
+        lists.add(shoppingList);
+        return ResponseEntity.ok(lists);
     }
 
     private ResponseEntity.BodyBuilder entityWithLocation(Object resourceId) {
